5bed63214ba4c9798bb2c366c5e4cfa1d01de96a
yjw <yjw@aexit.net>
2018-01-10 15:51:36 +0800

    添加vundleupdate出错解决提示

--- gVIM.bat
+++ gVIM.bat
@@ -1,5 +1,8 @@
-@ rem ECHO OFF
-@SETLOCAL
-@SET HOME=%~dp0
-@IF '%*' equ '' (@START /d "%~dp0vim80" gvim.exe -c "normal '0") ELSE (
-@START /d "%~dp0vim80" gvim.exe --remote-tab-silent %1)
+@ECHO OFF
+SETLOCAL
+SET HOME=%~dp0
+IF '%*' equ '' (@START /d "%~dp0vim80" gvim.exe -c "normal '0") ELSE (
+START /d "%~dp0vim80" gvim.exe --remote-tab-silent %1)
+
+@rem 如果启动后执行vundle更新时报错，则说明Vundle.vim版本信息不对，
+@rem 可以：cd .vim_runtime\my_plugins & git clone https://github.com/VundleVim/Vundle.vim.git
