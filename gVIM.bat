394bfdd220882a6a5498034474f3ff1a01ce73e9
yjw <yjw@aexit.net>
2018-05-10 11:17:20 +0800

    修改了一处问题clone插件vundle的错误

--- gVIM.bat
+++ gVIM.bat
@@ -1,8 +1,12 @@
 @ECHO OFF
-SETLOCAL
-SET HOME=%~dp0
-IF '%*' equ '' (@START /d "%~dp0vim80" gvim.exe -c "normal '0") ELSE (
-START /d "%~dp0vim80" gvim.exe --remote-tab-silent %1)
+rem 如果启动后执行vundle更新时报错，说明Vundle.vim版本信息不对
+rem cd .vim_runtime\my_plugins & git clone https://github.com/VundleVim/Vundle.vim.git
 
-@rem 如果启动后执行vundle更新时报错，则说明Vundle.vim版本信息不对，
-@rem 可以：cd .vim_runtime\my_plugins & git clone https://github.com/VundleVim/Vundle.vim.git
+SETLOCAL
+SET "HOME=%~dp0"
+SET "VIMROOT=%HOME:~,-1%\vim80"
+echo %VIMROOT%
+pause
+
+IF '%*' equ '' (@START /d "%VIMROOT%" gvim.exe -c "normal '0") ELSE (
+@START /d "%VIMROOT%" gvim.exe --remote-tab-silent %1)
