ec9a2299058632f5c680bb6bd7b2ea0812fa7032
yjw <yjw@aexit.net>
2019-03-28 16:35:32 +0800

    添加代理注释，修正vundle处的&为&&

--- gVIM.bat
+++ gVIM.bat
@@ -1,6 +1,26 @@
 @ECHO OFF
 rem 如果启动后执行vundle更新时报错，说明Vundle.vim版本信息不对
-rem cd .vim_runtime\my_plugins & git clone https://github.com/VundleVim/Vundle.vim.git
+rem cd .vim_runtime\my_plugins && git clone https://github.com/VundleVim/Vundle.vim.git
+
+rem 查看当前代理设置
+rem git config --global http.proxy
+
+rem 设置当前代理为 http://127.0.0.1:1080 或 socket5://127.0.0.1:1080
+rem git config --global http.proxy 'http://127.0.0.1:1080'
+rem git config --global https.proxy 'http://127.0.0.1:1080'
+rem git config --global http.proxy 'socks5://127.0.0.1:1080'
+rem git config --global https.proxy 'socks5://127.0.0.1:1080'
+rem 设置完成后在用户的主目录下可以看到.gitconfig文件中的变化
+rem 删除 proxy git config --global --unset http.proxy
+rem git config --global --unset https.proxy
+
+rem 设置SSH代理
+rem 我们也经常使用SSH来clone或者push项目，那么基于SSH的代理设置如下，首先在~/.ssh/中新建一个config文件，输入下面代码：
+
+rem Host github.com
+rem   HostName github.com
+rem   User git
+rem   ProxyCommand nc -v -x 127.0.0.1:1080 %h %p
 
 SETLOCAL
 SET "HOME=%~dp0"
